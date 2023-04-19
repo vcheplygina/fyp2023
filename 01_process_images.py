0c12126ef8013e4f95e9cec99ec0007d5a55d974
Veronika Cheplygina <v.cheplygina@gmail.com>
2023-04-19 10:06:26 +0200

    add classifier training/saving

--- 01_process_images.py
+++ 01_process_images.py
@@ -8,37 +8,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Import packages for image processing
-from skimage import morphology #for measuring things in the masks
-
-#-------------------
-# Help functions
-#-------------------
-
-
-#Main function to extract features from an image, that calls other functions    
-def extract_features(image):
-    
-    [r,g,b] = get_pixel_rgb(image)
-
-    #Here you need to add more of your custom-made functions for measuring features!
-
-    return np.array([r,g,b], dtype=np.float16)
-
-
-
-
-#Extracts the RGB values at location 100, 100. Example feature, probably not very useful
-def get_pixel_rgb(image):
-
-    x_coord = 100
-    y_coord = 100
-
-    r = image[x_coord, y_coord, 0]
-    g = image[x_coord, y_coord, 1]
-    b = image[x_coord, y_coord, 2]
-
-    return r,g,b
+# Import our own file that has the feature extraction functions
+from extract_features import extract_features
 
 
 
@@ -50,8 +21,8 @@ def get_pixel_rgb(image):
 #Where is the raw data
 file_data = '..' + os.sep + 'data' + os.sep +'metadata.csv'
 path_image = '..' + os.sep + 'data' + os.sep + 'images' + os.sep + 'imgs_part_1'
-
-
+    
+  
 #Where we will store the features
 file_features = 'features/features.csv'
 
@@ -59,43 +30,43 @@ file_features = 'features/features.csv'
 #Read meta-data into a Pandas dataframe
 df = pd.read_csv(file_data)
 
-# Extract image IDs and labels from the data
+# Extract image IDs and labels from the data. 
 image_id = list(df['img_id'])
 label = np.array(df['diagnostic'])
 
-# Here you could decide to filter the data in some way
+# Here you could decide to filter the data in some way (see task 0)
+# For example you can have a file selected_images.csv which stores the IDs of the files you need
 is_nevus =  label == 'NEV'
 
-
-#For now I just take the first 100...
-num_images = 100
+num_images = len(image_id)
 
 
 #Make array to store features
 feature_names = ['red','green','blue']
 num_features = len(feature_names)
-features = np.empty([num_images,num_features], dtype=np.float16)  
+features = np.zeros([num_images,num_features], dtype=np.float16)  
 
-#Loop through all images
-for i in np.arange(num_images):
+
+#Loop through all images (now just 10 for demonstration)
+for i in np.arange(10):
     
     # Define filenames related to this image
     file_image = path_image + os.sep + image_id[i] 
     
     if exists(file_image):
+        
         # Read the image
         im = plt.imread(file_image)
         im = np.float16(im)  
     
-        # Measure features - this does not do anything too useful yet!
+        # Measure features - this does not do anything useful yet!
         x = extract_features(im) 
            
         # Store in the variable we created before
-        features[i,0:num_features] = x
-
-
-
-#Save the features to a file      
+        features[i,:] = x
+       
+        
+#Save the image_id used + features to a file   
 df_features = pd.DataFrame(features, columns=feature_names)     
 df_features.to_csv(file_features, index=False)  
     
