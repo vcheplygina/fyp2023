35b119de9435724046d3220fe67724e881de0307
bTehe <66426840+bTehe@users.noreply.github.com>
2023-04-21 22:34:17 +0200

    added a segmentation algorithm

--- review.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-metdata = np.loadtxt("metadata.csv", delimiter=',', dtype=str)
-print(np.unique(metdata[:, 17])) # print all rows and 17th column
---
+++ segmentation_alg.py
@@ -0,0 +1,65 @@
+import numpy as np
+from skimage import io, filters, morphology, segmentation, color, img_as_ubyte, transform
+import matplotlib.pyplot as plt
+from skimage.draw import polygon
+
+def img_segm(path):
+
+  # reading a colourful image and resizing it
+  img_col = io.imread(path)
+  img_col = transform.resize(img_col, (img_col.shape[0] // 4, img_col.shape[1] // 4), anti_aliasing=True)
+
+
+  # reading a grayscale image and resizing it
+  image = io.imread(path, as_gray=True)
+  image = transform.resize(image, (image.shape[0] // 4, image.shape[1] // 4), anti_aliasing=True)
+  image = filters.gaussian(image)
+  
+
+  # thresholding the image 
+  thresholds = filters.threshold_multiotsu(image, classes=4)
+  regions = np.digitize(image, thresholds)
+  output = regions < 1
+
+
+  # making a circle
+  s = np.linspace(0, 2*np.pi, 270)   #Number of points on the circle
+  y = 210 + 140*np.sin(s)            #Row 
+  x = 210 + 150*np.cos(s)            #Column
+  init = np.array([y, x]).T
+
+  # and a snake
+  snake = segmentation.active_contour(output, init, w_line=0)
+
+
+  
+  # Find coordinates inside the polygon defined by the snake
+  rr, cc = polygon(init[:, 0], init[:, 1], output.shape)
+  mask = np.zeros_like(output)
+
+  # applying a mask on the polygon area
+  mask[rr, cc] = 1
+  cropped_img = output * mask[:, :]
+
+  # doing dilation
+  struct_el = morphology.disk(3)
+  mask_dilated = morphology.binary_dilation(cropped_img, struct_el)
+
+  # applying the mask and extracting the lesion from the image
+  im2 = img_col.copy()
+  im2[mask_dilated == 0] = 0
+
+
+  # plotting results
+  fig, ax = plt.subplots(1, 6, figsize=(15, 8))
+  ax[0].imshow(image, cmap='gray')
+  ax[1].imshow(output, cmap='gray')
+  ax[2].imshow(cropped_img, cmap='gray')
+  ax[3].imshow(mask_dilated, cmap='gray')
+  ax[4].imshow(regions, cmap='gray')
+  ax[5].imshow(im2, cmap='gray')          # resulting image
+  
+
+  # a circle and a snake for the second plot
+  ax[1].plot(init[:, 1], init[:, 0], '--r', lw=3)
+  ax[1].plot(snake[:, 1], snake[:, 0], '-b', lw=3)  
